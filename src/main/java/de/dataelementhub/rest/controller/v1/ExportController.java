160ced93aecf5e328840d430bbf3f656f34edbf2
AMMAR <ammarbr@hotmail.com>
2022-04-07 15:41:52 +0200

    #46 - Reconstruct export output and import input
    - Move ExportInfo and ExportRequest to datatransfer directory
    - Update StagingImport.json and StagingImport.xsd to validate stagedElement as object instead of array
    - Mark import label and stagedElements array as required

--- src/main/java/de/dataelementhub/rest/controller/v1/ExportController.java
+++ src/main/java/de/dataelementhub/rest/controller/v1/ExportController.java
@@ -1,7 +1,7 @@
 package de.dataelementhub.rest.controller.v1;
 
-import de.dataelementhub.model.dto.export.ExportInfo;
-import de.dataelementhub.model.dto.export.ExportRequest;
+import de.dataelementhub.model.dto.datatransfer.ExportInfo;
+import de.dataelementhub.model.dto.datatransfer.ExportRequest;
 import de.dataelementhub.model.service.ExportService;
 import de.dataelementhub.rest.DataElementHubRestApplication;
 import java.io.File;
