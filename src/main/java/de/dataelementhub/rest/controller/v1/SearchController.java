4d8d82a548b31244074eacfb6e9c4be82537b0b4
AMMAR <ammarbr@hotmail.com>
2022-07-12 15:49:36 +0200

    #78 - Validate elementType and status for search function
    - Validate input before starting the search process

--- src/main/java/de/dataelementhub/rest/controller/v1/SearchController.java
+++ src/main/java/de/dataelementhub/rest/controller/v1/SearchController.java
@@ -9,7 +9,9 @@ import de.dataelementhub.model.service.SearchService;
 import de.dataelementhub.rest.DataElementHubRestApplication;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 import org.jooq.DSLContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
@@ -46,37 +48,49 @@ public class SearchController {
    */
   @GetMapping
   @Order(SecurityProperties.BASIC_AUTH_ORDER)
-  public ResponseEntity<List<Element>> search(
+  public ResponseEntity<List<? extends Object>> search(
       @RequestParam(value = "query", required = true) String searchText,
       @RequestParam(value = "type", required = false) List<String> typesString,
       @RequestParam(value = "status", required = false) List<String> statusString,
       @RequestParam(value = "section", required = false) List<String> elementPartsString
   ) {
     List<ElementType> types = new ArrayList<>();
-    if (typesString != null) {
-      for (String ts : typesString) {
-        types.add(ElementType.valueOf(ts));
-      }
-    } else {
-      types = Arrays.asList(ElementType.values());
-    }
-
     List<Status> status = new ArrayList<>();
-    if (statusString != null) {
-      for (String s : statusString) {
-        status.add(Status.valueOf(s));
+    List<String> availableElementPartsString = Arrays.asList("definition", "designation", "slotkey",
+        "slotvalue", "conceptassociationsystem", "conceptassociationterm", "conceptassociationtext",
+        "datatype", "valuedomaindescription", "unitofmeasure", "format");
+    try {
+      if (typesString != null) {
+        for (String ts : typesString) {
+          types.add(ElementType.valueOf(ts));
+        }
+      } else {
+        types = Arrays.asList(ElementType.values());
       }
-    } else {
-      status = Arrays.asList(Status.values());
-    }
 
-    // Insert default values if no restriction was set via query param
-    if (elementPartsString == null) {
-      elementPartsString = Arrays.asList("definition", "designation", "slotKey", "slotValue",
-          "conceptAssociationSystem", "conceptAssociationTerm", "conceptAssociationText",
-          "dataType", "valueDomainDescription", "unitOfMeasure", "format");
-    }
 
+      if (statusString != null) {
+        for (String s : statusString) {
+          status.add(Status.valueOf(s));
+        }
+      } else {
+        status = Arrays.asList(Status.values());
+      }
+
+      // Insert default values if no restriction was set via query param
+      if (elementPartsString == null) {
+        elementPartsString = availableElementPartsString;
+      } else {
+        elementPartsString = elementPartsString.stream().map(String::toLowerCase).collect(
+            Collectors.toList());
+        if (!availableElementPartsString.containsAll(elementPartsString)) {
+          throw new IllegalArgumentException("Element parts are not valid!");
+        }
+      }
+    } catch (IllegalArgumentException iae) {
+      return new ResponseEntity<>(
+          Collections.singletonList(iae.getMessage()), HttpStatus.UNPROCESSABLE_ENTITY);
+    }
     int userId = UserHandler.getUserByIdentity(ctx,
         DataElementHubRestApplication.getCurrentUserName()).getId();
     SearchRequest searchRequest = new SearchRequest(searchText, types, status, elementPartsString);
