0bcd2b62013ce5ce6069483b1961d0e8c4d3f0a2
Larie <larie.borelle.siabou@ges.thm.de>
2025-10-16 17:10:47 +0200

    #defined Value Domain
    Search modifications at concept association
    
    -Allow UMLS search in specific Sources
    -allow SemlookP serach in all Ontologies

--- src/main/java/de/dataelementhub/rest/controller/v1/ConceptAssociationController/SemlookpController.java
+++ src/main/java/de/dataelementhub/rest/controller/v1/ConceptAssociationController/SemlookpController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
 import java.util.List;
 
 import static de.dataelementhub.rest.controller.v1.ApiVersion.API_VERSION;
@@ -13,18 +14,25 @@ import static de.dataelementhub.rest.controller.v1.ApiVersion.API_VERSION;
 @RestController
 @RequestMapping(API_VERSION + "/semlookp")
 public class SemlookpController {
-    private final SemlookpService lookupService;
+    private final SemlookpService semlookpService;
 
     public SemlookpController(SemlookpService lookupService) {
-        this.lookupService = lookupService;
+        this.semlookpService = lookupService;
     }
 
+    // Search in a specific ontology
     @GetMapping("/search")
     public List<SemlookpService.Concept> searchConcepts(@RequestParam String query, String ontology) {
-        return lookupService.searchInOntology(query, ontology);
+        return semlookpService.searchInOntology(query, ontology);
+    }
+    //search in all Ontologies
+    @GetMapping("/searchInAllOntology")
+    public List<SemlookpService.Concept> searchConcepts(
+            @RequestParam("query") String query) throws IOException {
+        return semlookpService.searchAllOntologies(query);
     }
     @GetMapping("/ids")
     public List<String> getOntologyIds() {
-        return lookupService.getAllOntologyIds();
+        return semlookpService.getAllOntologyIds();
     }
 }
